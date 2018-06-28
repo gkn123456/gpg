8a295b2c9e9dde064c56b5330d908a028742f248
gkn123456 <885303628@qq.com>
2018-06-28 21:42:28 +0800

    sxl-first

---
+++ js/index.js
@@ -0,0 +1,33 @@
+$(function(){
+
+	$(".xl ul .ch:eq(0)").click(function(){
+		$(".xl ul:eq(0)").toggleClass("hot");
+		$(".xl ul .ch:eq(0)").toggleClass("ho");
+	})
+	$(".xl ul .ch:eq(1)").click(function(){
+		$(".xl ul:eq(1)").toggleClass("hot");
+		$(".xl ul .ch:eq(1)").toggleClass("ho");
+	})
+	$(".xl ul .ch:eq(2)").click(function(){
+		$(".xl ul:eq(2)").toggleClass("hot");
+		$(".xl ul .ch:eq(2)").toggleClass("ho");
+	})
+	$(".xl ul .ch:eq(3)").click(function(){
+		$(".xl ul:eq(3)").toggleClass("hot");
+		$(".xl ul .ch:eq(3)").toggleClass("ho");
+	})
+	$(".xl ul .ch:eq(4)").click(function(){
+		$(".xl ul:eq(4)").toggleClass("hot");
+		$(".xl ul .ch:eq(4)").toggleClass("ho");
+	})
+	$(".xl ul .ch:eq(5)").click(function(){
+		$(".xl ul:eq(5)").toggleClass("hot");
+		$(".xl ul .ch:eq(5)").toggleClass("ho");
+	})
+	$(".xl ul .ch:eq(6)").click(function(){
+		$(".xl ul:eq(6)").toggleClass("hot");
+		$(".xl ul .ch:eq(6)").toggleClass("ho");
+	})
+	
+	
+})
